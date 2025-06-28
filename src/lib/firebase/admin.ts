221dc317362702cf03d44d9c82035bde9ededa86
Hassan Abdulaziz <hassanabdulaziz106@gmail.com>
2025-06-28 18:27:05 +0000

    prolonged error : [14:20:15.374] Running build in Washington, D.C., USA

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -23,6 +23,7 @@ function initializeFirebaseAdmin() {
       admin.initializeApp({
         credential: admin.credential.cert({
           projectId: process.env.FIREBASE_PROJECT_ID,
+          // Vercel escapes newlines, so we need to replace them back
           privateKey: (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
           clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         }),
